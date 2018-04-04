6f1042d0eae104f8830c3e3e2dc152854ad78ed2
Michael van Meerwijk <michaelvanm@gmail.com>
2018-04-04 19:53:45 +0200

    bookmark delete and edit icons

--- src/components/BookmarkEdit.vue
+++ src/components/BookmarkEdit.vue
@@ -1,8 +1,20 @@
 <template>
 <div class="bookmark-edit">
 	<div class="bookmark-edit-buttons">
-		<button @click="deleteBookmark">Delete</button>
-		<button @click="startBookmarkEditing">Edit</button>
+		<button @click="deleteBookmark">
+			<svg class="delete-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+				<path d="M0 0h24v24H0z" fill="none"/>
+			</svg>
+			Delete
+		</button>
+		<button @click="startBookmarkEditing">
+			<svg class="edit-mode-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+				<path d="M0 0h24v24H0z" fill="none"/>
+			</svg>
+			Edit
+		</button>
 	</div>	
 </div>
 </template>
@@ -36,9 +48,17 @@ export default {
 
 .bookmark-edit-buttons > button {
 	padding: 0.25em 0.75em;
+	display: inline-flex;
+	align-items: center;
 }
 
 .bookmark-edit-buttons > button:not(:first-of-type) {
 	margin-left: 1em;
+}
+
+.edit-mode-icon, .delete-icon {
+	fill: currentColor;
+	width: 1.25em;
+	height: 1.25em;
 }
 </style>
