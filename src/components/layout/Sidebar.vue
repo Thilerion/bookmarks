3c1544ee8093f93bd11c4ef56421ceb4210e6ec4
Michael van Meerwijk <michaelvanm@gmail.com>
2018-04-23 16:44:16 +0200

    main layout, header, subheader

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,4 +1,59 @@
 <template>
+<aside class="sidebar">
+	<header>
+		<svg version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			x="0px" y="0px"
+			viewBox="0 0 54 54" xml:space="preserve"
+			class="site-icon"
+		>
+			<path style="fill:rgba(200,50,50,1);" d="M8.589,0C5.779,0,3.5,2.279,3.5,5.089V54l18-12l18,12V6c0-3.3,2.7-6,6-6H8.589z"/>
+			<path style="fill:rgba(200,50,50,0.6);" d="M45.41,0.005C42.151,0.054,39.5,2.73,39.5,6v17h11V5.135C50.5,2.315,48.225,0.03,45.41,0.005z"/>
+		</svg>
+		<h1 class="site-heading">Bookmarks</h1>
+	</header>
+	
+	<BmSidebarSubHeader class="sub-header" />
+
+	<div class="sidebar-content">
+		Main sidebar content here
+		<ul>
+			<li>Lorem ipsum dolor sit amet.</li>
+			<li>Accusantium veritatis nisi cumque repellat!</li>
+			<li>Praesentium, culpa! Libero, nobis voluptatum.</li>
+			<li>Ex dolorum recusandae earum aliquam?</li>
+			<li>Corrupti dolores velit nulla vitae.</li>
+			<li>Eligendi suscipit ex sunt rerum.</li>
+			<li>Consequuntur possimus quisquam non perspiciatis.</li>
+			<li>Vitae illum quod exercitationem corporis.</li>
+			<li>Tenetur odit consectetur alias optio!</li>
+			<li>Obcaecati harum tempore rem facere?</li>
+			<li>Earum necessitatibus temporibus provident totam?</li>
+			<li>Perspiciatis eum quidem perferendis? Aliquid?</li>
+			<li>Dignissimos ducimus quam rerum odio.</li>
+			<li>Numquam officia repellendus sunt consequuntur?</li>
+			<li>Reprehenderit soluta autem ea exercitationem?</li>
+			<li>Debitis quibusdam veniam rerum ex.</li>
+			<li>Enim deleniti modi eius laboriosam.</li>
+			<li>At dicta atque consequatur perferendis!</li>
+			<li>Laudantium veniam placeat repudiandae magnam.</li>
+			<li>Nam a expedita doloremque nihil.</li>
+			<li>Aperiam necessitatibus voluptatibus sunt iusto.</li>
+			<li>Laboriosam veritatis sapiente voluptatum officiis.</li>
+			<li>Laborum sapiente eius omnis ratione!</li>
+			<li>Alias, repudiandae. Tempore, vero quasi.</li>
+			<li>Nihil eius quia sequi iusto.</li>
+			<li>Doloremque, soluta voluptas! Nesciunt, magnam?</li>
+			<li>Dolore aut quibusdam temporibus nobis.</li>
+			<li>Modi neque totam sunt sapiente.</li>
+			<li>Neque accusamus dolore odit nam.</li>
+			<li>Facilis, nisi. Tempore, quidem maxime.</li>
+		</ul>
+	</div>
+</aside>
+
+<!--
 <div class="sidebar">
 	<div class="tag-header">
 		<h2>Tags</h2>
@@ -17,10 +72,18 @@
 	<transition name="collapse-tag-list">
 		<BmTagList class="tags-list" v-if="showTagList" ref="tagList" />
 	</transition>
-</div>
+</div>-->
 </template>
 
 <script>
+import SidebarSubHeader from './SidebarSubHeader'
+
+export default {
+	components: {
+		BmSidebarSubHeader: SidebarSubHeader
+	}
+}
+/*
 import TagList from '../tag-list/TagList';
 import SvgIcon from '../shared/SvgIcon';
 
@@ -56,9 +119,45 @@ export default {
 		}
 	}
 }
+*/
 </script>
 
-<style scoped>
+<style>
+h1.site-heading {
+	letter-spacing: 1px;
+	font-size: 2.25em;
+	text-transform: capitalize;
+	display: inline-block;
+	padding-left: .5em;
+}
+
+.site-icon {
+	height: 28px;
+}
+
+.sidebar {	
+	display: flex;
+	flex-direction: column;	
+}
+
+header {
+	background: #333;
+	color: white;
+	text-align: center;
+	padding: 2em 0;
+	cursor:default;
+}
+
+.sub-header {
+	background: #444;
+	color: white;
+}
+
+.sidebar-content {
+	overflow-y: scroll;
+	overflow-x: hidden;
+}
+/*
 h2 {
 	font-size: 1em;
 	font-weight: bold;
@@ -126,5 +225,5 @@ h2 {
 	transform: translateY(-20em);
 	opacity: 0;
 }
-
+*/
 </style>
---
+++ src/components/layout/SidebarSubHeader.vue
@@ -0,0 +1,82 @@
+<template>
+<div>
+	<button class="add-bm-btn" @click="toggleModalAddBookmark">
+		<BmSvgIcon icon="plus"/>
+		<span class="add-bm-text">Add bookmark</span>
+	</button>
+</div>
+</template>
+
+<script>
+import SvgIcon from '../shared/SvgIcon';
+export default {
+	components: {
+		BmSvgIcon: SvgIcon
+	},
+	methods: {
+		toggleModalAddBookmark() {
+			this.$store.commit('enableModal', 0);
+		}
+	}
+}
+</script>
+
+<style>
+.add-bm-btn {
+	width: 100%;
+	text-align: left;
+	padding: 1em;
+	display: flex;
+	align-items: center;
+	color: white;
+	border: none;
+	position: relative;
+}
+
+.add-bm-btn:not(:last-of-type)::after {
+	background: rgba(255,255,255,0.2);
+	display: block;
+	height: 1px;
+	content: "";
+	position: absolute;
+	bottom: 0;
+	left: 1em;
+	right: 1em;
+}
+
+.add-bm-btn:hover .add-bm-text {
+	text-decoration: underline;
+}
+
+.add-bm-text {
+	font-size: 1em;
+	margin-left: 1em;
+	font-weight: bold;
+	letter-spacing: 0.025em;
+}
+
+.add-icon {
+	width: 1.35em;
+	height: 1.35em;
+}
+
+/*
+.sub-header-item button {
+	padding: 1em 0;
+	width: 100%;
+	text-align: left;
+	font-size: inherit;
+	display: flex;
+	align-items: center;
+}
+
+.add-button-text {
+	font-size: 1em;
+	margin-left: 1em;
+}
+
+.sub-header-item .add-icon {
+	width: 1.35em;
+	height: 1.35em;
+}*/
+</style>
