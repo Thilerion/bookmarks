1608625e0c3cffcc4cfbeede6e4f1dc5a9b2f0f8
Michael van Meerwijk <michaelvanm@gmail.com>
2018-04-29 20:06:17 +0200

    normal list base

--- src/components/bookmarks/view/BookmarkNormal.vue
+++ src/components/bookmarks/view/BookmarkNormal.vue
@@ -26,8 +26,8 @@
 		@deleteBookmark="deleteBookmark"
 		@editBookmark="editBookmark"
 	/>-->
-
-	<div class="left">
+	<!--
+	<div class="col1">
 		<a
 			:href="url"
 			class="title"
@@ -43,9 +43,9 @@
 			<button class="delete">Delete</button>
 		</div>		
 	</div>
-	<div class="right" :class="{'align-middle': !showCategory}">
+	<div class="col2" :class="{'align-middle': !showCategory}">
 		<p
-			class="bm-sub bm-date"
+			class="bm-sub date"
 			:title="dateTotalString"
 		>{{dateAdded | filterDateString}}</p>
 		<BmCategoryDisplay
@@ -53,6 +53,35 @@
 			class="cat-display"		
 			v-if="showCategory"
 		/>
+	</div>
+	<div class="col3">
+
+	</div>
+	-->
+	<div class="col-bm-main">
+		<a
+			:href="url"
+			class="title"
+			target="_blank"
+			v-html="$options.filters.filterHighlight(title, searchTerm)"
+		></a>
+		<p class="description">Description comes here, if applicable</p>
+		<p class="tags">Tags come here, if there are any</p>
+		<div class="row-options">
+			<button>Star</button>
+			<button>Edit</button>
+			<button>Delete</button>
+		</div>
+	</div>
+	<div class="col-cat">
+		<BmCategoryDisplay
+			:catId="catId"
+			class="cat-display"		
+			v-if="showCategory"
+		/>
+	</div>
+	<div class="col-options">
+		<button>More</button>
 	</div>
 
 </div>
@@ -116,90 +145,6 @@ export default {
 	display: flex;
 	border: 1px solid transparent;
 	padding: 0.5em 1em;
-	height: 5em;
-}
-
-.bm-sub {
-	font-size: 0.85em;
-}
-
-.left {
-	flex: 1 0 1em;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	min-width: 1em;
-	overflow: hidden;
-}
-
-.right {
-	display: flex;
-	flex-direction: column;
-	align-items: flex-end;
-	justify-content: space-between;
-	flex: 0 0 auto;
-}
-
-.date {
-	flex: 0 0 auto;
-}
-
-.right.align-middle {
-	justify-content: center;
-}
-
-.title {
-	display: inline-block;
-	flex: 0 0 1rem;
-	min-width: 1em;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
-}
-
-.url {
-	flex: 0 0 1rem;
-}
-
-.options {
-	flex: 0 0 1rem;
-	display: flex;
-	align-items: flex-end;	
-	opacity: 0.5;
-}
-
-.options > button {
-	font-style: italic;
-	letter-spacing: 0.2px;
-}
-
-.options .dash {
-	margin: 0 0.5em;	
-}
-
-.bm-item:not(:last-of-type) {
-	border-bottom: 1px solid var(--border-transparent-darken);
-}
-
-.bm-item:first-of-type {
-	border-radius: 5px 5px 0 0;
-}
-
-.bm-item:last-of-type {
-	border-radius: 0 0 5px 5px;
-}
-
-.bm-item:hover {
-	border-left-color: var(--border-transparent-darken);
-	border-right-color: var(--border-transparent-darken);
-}
-
-.bm-item:first-of-type:hover {
-	border-top-color: var(--border-transparent-darken);;
-}
-
-.bm-item:last-of-type:hover {
-	border-bottom-color: var(--border-transparent-darken);;
 }
 
 .cat-display {
--- src/components/bookmarks/view/BookmarksListNormal.vue
+++ src/components/bookmarks/view/BookmarksListNormal.vue
@@ -31,6 +31,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.bm-item:not(:last-of-type) {
+	border-bottom: 1px solid var(--border-transparent-darken);
+}
 
+.bm-item:first-of-type {
+	border-radius: 5px 5px 0 0;
+}
+
+.bm-item:last-of-type {
+	border-radius: 0 0 5px 5px;
+}
+
+.bm-item:hover {
+	border-left-color: var(--border-transparent-darken);
+	border-right-color: var(--border-transparent-darken);
+}
+
+.bm-item:first-of-type:hover {
+	border-top-color: var(--border-transparent-darken);;
+}
+
+.bm-item:last-of-type:hover {
+	border-bottom-color: var(--border-transparent-darken);;
+}
 </style>
